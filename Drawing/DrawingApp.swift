f1c9a88acdae6393a6fbb2dc8026abbd12a9c75b
Julia <js.shinkareva@gmail.com>
2023-05-22 08:51:00 +0200

    drawing, version1

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by yulias on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
